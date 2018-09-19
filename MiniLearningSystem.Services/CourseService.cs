ec2bcec5aad684e1cd1276809ed1af3801957aac
RBkaTa <qvor007@abv.bg>
2018-09-19 21:02:12 +0300

    added logic

---
+++ MiniLearningSystem.Services/CourseService.cs
@@ -0,0 +1,18 @@
+﻿using MiniLearningSystem.Models.EntityModels;
+using System.Data.Entity;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MiniLearningSystem.Services
+{
+    public class CourseService : Service
+    {
+        public ICollection<Course> GetAll()
+        {
+            using (this.Context)
+            {
+                return this.Context.Courses.Include(c => c.Trainer).Include(c => c.Students).ToList();
+            }
+        }
+    }
+}
--- MiniLearningSystem/Controllers/HomeController.cs
+++ MiniLearningSystem/Controllers/HomeController.cs
@@ -1,11 +1,21 @@
-﻿using System.Web.Mvc;
+﻿using MiniLearningSystem.Services;
+using System.Web.Mvc;
 
 namespace MiniLearningSystem.Controllers
 {
     public class HomeController : Controller
     {
+        private CourseService _courseService;
+
+        public HomeController()
+        {
+            _courseService = new CourseService();
+        }
+
         public ActionResult Index()
         {
+            var courses = _courseService.GetAll();
+
             return View();
         }
 
